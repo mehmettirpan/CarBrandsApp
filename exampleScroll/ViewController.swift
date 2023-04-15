051ba7c12956bb87439702cb1a159bfc2ac86cbb
Mehmet Tirpan <mehmettirpan92@gmail.com>
2023-04-16 00:45:32 +0300

    first commit

---
+++ exampleScroll/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  exampleScroll
+//
+//  Created by Mehmet TIRPAN on 15.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+
+    
+    @IBOutlet weak var volkswagen: UIButton!
+    
+    @IBOutlet weak var renault: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "Brands"
+    }
+
+    
+    
+
+}
+
